002cb8198064a142e862cf94df79994360459798
Jesus Garcia A <jesusgq08@hotmail.com>
2026-01-09 23:00:18 +0100

    Main

--- WindowsVM/Config/config.php
+++ WindowsVM/Config/config.php
@@ -4,6 +4,5 @@ return [
     'name' => 'WindowsVM',
     'author' => 'TheXHosting',
     'version' => '1.0.0',
-    // Set this to your current WemX version if you want strict compatibility checks
     'wemx_version' => '>=1.0.0',
 ];
